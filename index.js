4668f363284b02f206e4891bf5d32d761b90649f
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-02-20 10:07:27 +0100

    moved App

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import createLogger from 'redux-logger';
 import {AppContainer} from 'react-hot-loader';
 
 import reducers from './src/reducers';
-import App from './src/components/App';
+import App from './src/container/App';
 
 import ons from 'onsenui';
 import 'onsenui/css/onsenui.css';
@@ -34,8 +34,8 @@ ons.ready(() => render(
 ));
 
 if (module.hot) {
-  module.hot.accept('./src/components/App', () => {
-    const NextApp = require('./src/components/App').default;
+  module.hot.accept('./src/container/App', () => {
+    const NextApp = require('./src/container/App').default;
     render(
       <AppContainer>
         <Provider store={store}>

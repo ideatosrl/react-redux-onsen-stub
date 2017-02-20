b510527fd714c38c77ff52337776ab849b84d653
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-02-20 12:29:09 +0100

    added pre-commit hook

---
+++ scripts/pre-commit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for file in $(git diff --cached --name-only --diff-filter=d | grep -E '\.(js|jsx)$')
+do
+  git show ":$file" | www/app/node_modules/.bin/eslint --stdin --stdin-filename "$file" # we only want to lint the staged changes, not any un-staged changes
+  if [ $? -ne 0 ]; then
+    echo "ESLint fallito sul file '$file'. Per cortesia controlla meglio il tuo codice e riprova."
+    exit 1 # exit with failure status
+  fi
+done
